dc040d241281f7e5b4d212781e5da5b75b4589bf
Codi Bezouka-Smith <lowheartrate@users.noreply.github.com>
2017-09-04 05:42:36 -0400

    Added config.lua to client_scripts & more
    Added config.lua to client_scripts and also set a description the script.

--- streetLabel/__resource.lua
+++ streetLabel/__resource.lua
@@ -1,3 +1,11 @@
+-- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-client_script 'client.lua'
+-- Script Description
+description 'Street Label byLowheartrate'
+
+-- Client Scripts
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
