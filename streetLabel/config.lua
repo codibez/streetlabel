dae9d247bd3f02905d95f5d46433c76695da5a5c
Codi Bezouka-Smith <lowheartrate@users.noreply.github.com>
2017-09-04 05:40:14 -0400

    Created config.lua
    This config.lua allows users to adjust the position of the HUD to their liking. The X & Y variables will be used in the client.lua file to properly adjust the position of each element for each direction.

---
+++ streetLabel/config.lua
@@ -0,0 +1,6 @@
+-- Use the following variable(s) to adjust the position.
+	-- adjust the x-axis (left/right)
+	x = 1.000
+	-- adjust the y-axis (top/bottom)
+	y = 1.000
+-- If you do not see the HUD after restarting script you adjusted the x/y axis too far.
