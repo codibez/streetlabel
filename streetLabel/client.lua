6bc07e4e1578c2917102a606c7a3e785c9b8b8a2
Matt Smith <psykzz@users.noreply.github.com>
2017-10-30 19:38:29 +0000

    Github spaces doesn't like my IDE

--- streetLabel/client.lua
+++ streetLabel/client.lua
@@ -1,28 +1,28 @@
 function drawTxt(x, y, width, height, scale, text, r, g, b, a)
-    SetTextFont(4)
-    SetTextProportional(0)
-    SetTextScale(scale, scale)
-    SetTextColour(r, g, b, a)
-    SetTextDropShadow(0, 0, 0, 0,255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextDropShadow()
-    SetTextOutline()
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(x - width/2, y - height/2 + 0.005)
+  SetTextFont(4)
+  SetTextProportional(0)
+  SetTextScale(scale, scale)
+  SetTextColour(r, g, b, a)
+  SetTextDropShadow(0, 0, 0, 0,255)
+  SetTextEdge(1, 0, 0, 0, 255)
+  SetTextDropShadow()
+  SetTextOutline()
+  SetTextEntry("STRING")
+  AddTextComponentString(text)
+  DrawText(x - width/2, y - height/2 + 0.005)
 end
 function drawTxt2(x, y, width, height, scale, text, r, g, b, a)
-    SetTextFont(6)
-    SetTextProportional(0)
-    SetTextScale(scale, scale)
-    SetTextColour(r, g, b, a)
-    SetTextDropShadow(0, 0, 0, 0,255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextDropShadow()
-    SetTextOutline()
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(x - width/2, y - height/2 + 0.005)
+  SetTextFont(6)
+  SetTextProportional(0)
+  SetTextScale(scale, scale)
+  SetTextColour(r, g, b, a)
+  SetTextDropShadow(0, 0, 0, 0,255)
+  SetTextEdge(1, 0, 0, 0, 255)
+  SetTextDropShadow()
+  SetTextOutline()
+  SetTextEntry("STRING")
+  AddTextComponentString(text)
+  DrawText(x - width/2, y - height/2 + 0.005)
 end
 local zones = { ['AIRP'] = "Los Santos International Airport", ['ALAMO'] = "Alamo Sea", ['ALTA'] = "Alta", ['ARMYB'] = "Fort Zancudo", ['BANHAMC'] = "Banham Canyon Dr", ['BANNING'] = "Banning", ['BEACH'] = "Vespucci Beach", ['BHAMCA'] = "Banham Canyon", ['BRADP'] = "Braddock Pass", ['BRADT'] = "Braddock Tunnel", ['BURTON'] = "Burton", ['CALAFB'] = "Calafia Bridge", ['CANNY'] = "Raton Canyon", ['CCREAK'] = "Cassidy Creek", ['CHAMH'] = "Chamberlain Hills", ['CHIL'] = "Vinewood Hills", ['CHU'] = "Chumash", ['CMSW'] = "Chiliad Mountain State Wilderness", ['CYPRE'] = "Cypress Flats", ['DAVIS'] = "Davis", ['DELBE'] = "Del Perro Beach", ['DELPE'] = "Del Perro", ['DELSOL'] = "La Puerta", ['DESRT'] = "Grand Senora Desert", ['DOWNT'] = "Downtown", ['DTVINE'] = "Downtown Vinewood", ['EAST_V'] = "East Vinewood", ['EBURO'] = "El Burro Heights", ['ELGORL'] = "El Gordo Lighthouse", ['ELYSIAN'] = "Elysian Island", ['GALFISH'] = "Galilee", ['GOLF'] = "GWC and Golfing Society", ['GRAPES'] = "Grapeseed", ['GREATC'] = "Great Chaparral", ['HARMO'] = "Harmony", ['HAWICK'] = "Hawick", ['HORS'] = "Vinewood Racetrack", ['HUMLAB'] = "Humane Labs and Research", ['JAIL'] = "Bolingbroke Penitentiary", ['KOREAT'] = "Little Seoul", ['LACT'] = "Land Act Reservoir", ['LAGO'] = "Lago Zancudo", ['LDAM'] = "Land Act Dam", ['LEGSQU'] = "Legion Square", ['LMESA'] = "La Mesa", ['LOSPUER'] = "La Puerta", ['MIRR'] = "Mirror Park", ['MORN'] = "Morningwood", ['MOVIE'] = "Richards Majestic", ['MTCHIL'] = "Mount Chiliad", ['MTGORDO'] = "Mount Gordo", ['MTJOSE'] = "Mount Josiah", ['MURRI'] = "Murrieta Heights", ['NCHU'] = "North Chumash", ['NOOSE'] = "N.O.O.S.E", ['OCEANA'] = "Pacific Ocean", ['PALCOV'] = "Paleto Cove", ['PALETO'] = "Paleto Bay", ['PALFOR'] = "Paleto Forest", ['PALHIGH'] = "Palomino Highlands", ['PALMPOW'] = "Palmer-Taylor Power Station", ['PBLUFF'] = "Pacific Bluffs", ['PBOX'] = "Pillbox Hill", ['PROCOB'] = "Procopio Beach", ['RANCHO'] = "Rancho", ['RGLEN'] = "Richman Glen", ['RICHM'] = "Richman", ['ROCKF'] = "Rockford Hills", ['RTRAK'] = "Redwood Lights Track", ['SANAND'] = "San Andreas", ['SANCHIA'] = "San Chianski Mountain Range", ['SANDY'] = "Sandy Shores", ['SKID'] = "Mission Row", ['SLAB'] = "Stab City", ['STAD'] = "Maze Bank Arena", ['STRAW'] = "Strawberry", ['TATAMO'] = "Tataviam Mountains", ['TERMINA'] = "Terminal", ['TEXTI'] = "Textile City", ['TONGVAH'] = "Tongva Hills", ['TONGVAV'] = "Tongva Valley", ['VCANA'] = "Vespucci Canals", ['VESP'] = "Vespucci", ['VINE'] = "Vinewood", ['WINDF'] = "Ron Alternates Wind Farm", ['WVINE'] = "West Vinewood", ['ZANCUDO'] = "Zancudo River", ['ZP_ORT'] = "Port of South Los Santos", ['ZQ_UAR'] = "Davis Quartz" }
 local directions = { [0] = 'N', [45] = 'NW', [90] = 'W', [135] = 'SW', [180] = 'S', [225] = 'SE', [270] = 'E', [315] = 'NE', [360] = 'N', }
@@ -41,36 +41,36 @@ local text_offset = 0.290
 
 Citizen.CreateThread(function()
   while true do
-  Citizen.Wait(1)
-  local pos = GetEntityCoords(GetPlayerPed(-1))
-  local var1, var2 = GetStreetNameAtCoord(pos.x, pos.y, pos.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
+    Citizen.Wait(1)
+    local pos = GetEntityCoords(GetPlayerPed(-1))
+    local var1, var2 = GetStreetNameAtCoord(pos.x, pos.y, pos.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
 
-  local var1_name = GetStreetNameFromHashKey(var1)
-  local var2_name = GetStreetNameFromHashKey(var2)
+    local var1_name = GetStreetNameFromHashKey(var1)
+    local var2_name = GetStreetNameFromHashKey(var2)
 
     local zone_name = GetNameOfZone(pos.x, pos.y, pos.z)
-  local current_zone = zones[zone_name]
+    local current_zone = zones[zone_name]
 
-  for k,v in pairs(directions)do
-  direction = GetEntityHeading(GetPlayerPed(-1))
-  if(math.abs(direction - k) < 22.5)then
-  direction = v
-  break;
-  end
-  end
-
-  if(var1_name and zone_name)then
-  if(zones[zone_name] and tostring(var1_name))then
-    drawTxt(x-0.345, y+0.66, 1.0,1.5,1.4, " | ", border_r, border_g, border_b, border_a)
-    drawTxt(x-0.310, y+0.66, 1.0,1.5,1.4, " | ", border_r, border_g, border_b, border_a)
-    drawTxt(x-0.330 + direction_offset[direction], y + 0.42, 1.0, 1.0, 1.0, direction, dir_r, dir_g, dir_b, dir_a)
-    if tostring(var2_name) == "" then
-      drawTxt2(x - text_offset, y + 0.45, 1.0,1.0,0.45, current_zone, town_r, town_g, town_b, town_a)
-    else
-      drawTxt2(x - text_offset, y + 0.45, 1.0,1.0,0.45, tostring(var2_name) .. ", " .. zones[zone_name], str_around_r, str_around_g, str_around_b, str_around_a)
+    for k,v in pairs(directions)do
+      direction = GetEntityHeading(GetPlayerPed(-1))
+      if(math.abs(direction - k) < 22.5)then
+        direction = v
+        break;
+      end
     end
-    drawTxt2(x - text_offset, y + 0.42, 1.0,1.0,0.55, tostring(var1_name), curr_street_r, curr_street_g, curr_street_b, curr_street_a)
-  end
-  end
+
+    if(var1_name and zone_name)then
+      if(zones[zone_name] and tostring(var1_name))then
+        drawTxt(x-0.345, y+0.66, 1.0,1.5,1.4, " | ", border_r, border_g, border_b, border_a)
+        drawTxt(x-0.310, y+0.66, 1.0,1.5,1.4, " | ", border_r, border_g, border_b, border_a)
+        drawTxt(x-0.330 + direction_offset[direction], y + 0.42, 1.0, 1.0, 1.0, direction, dir_r, dir_g, dir_b, dir_a)
+        if tostring(var2_name) == "" then
+          drawTxt2(x - text_offset, y + 0.45, 1.0,1.0,0.45, current_zone, town_r, town_g, town_b, town_a)
+        else
+          drawTxt2(x - text_offset, y + 0.45, 1.0,1.0,0.45, tostring(var2_name) .. ", " .. zones[zone_name], str_around_r, str_around_g, str_around_b, str_around_a)
+        end
+        drawTxt2(x - text_offset, y + 0.42, 1.0,1.0,0.55, tostring(var1_name), curr_street_r, curr_street_g, curr_street_b, curr_street_a)
+      end
+    end
   end
 end)
