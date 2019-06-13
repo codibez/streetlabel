5577fac7df36c1664ddc64249706db86028f8b40
Codi Bezouka-Smith <lowheartrate@users.noreply.github.com>
2019-06-13 03:04:31 -0400

    Updated manifest
    Updated to most recent manifest version.

--- streetLabel/__resource.lua
+++ streetLabel/__resource.lua
@@ -1,5 +1,5 @@
 -- Manifest
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 -- Script Description
 description 'Street Label byLowheartrate'
