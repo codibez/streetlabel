a38aef25016ed3bfa9254dcc5f7691cbb578dc1f
Logan Elliott <illusivetea@outlook.com>
2019-07-07 16:03:57 +0100

    Add new config setting

--- streetLabel/config.lua
+++ streetLabel/config.lua
@@ -1,40 +1,44 @@
 -- Use the following variable(s) to adjust the position.
-	-- adjust the x-axis (left/right)
-	x = 1.000
-	-- adjust the y-axis (top/bottom)
-	y = 1.000
+-- adjust the x-axis (left/right)
+x = 1.000
+-- adjust the y-axis (top/bottom)
+y = 1.000
+
 -- If you do not see the HUD after restarting script you adjusted the x/y axis too far.
 	
 -- Use the following variable(s) to adjust the color(s) of each element.
-	-- Use the following variables to adjust the color of the border around direction.
-	border_r = 255
-	border_g = 255
-	border_b = 255
-	border_a = 100
-	
-	-- Use the following variables to adjust the color of the direction user is facing.
-	dir_r = 255
-	dir_g = 255
-	dir_b = 255
-	dir_a = 255
-	
-	-- Use the following variables to adjust the color of the street user is currently on.
-	curr_street_r = 240
-	curr_street_g = 200
-	curr_street_b = 80
-	curr_street_a = 255
-	
-	-- Use the following variables to adjust the color of the street around the player. (this will also change the town the user is in)
-	str_around_r = 255
-	str_around_g = 255 
-	str_around_b = 255
-	str_around_a = 255
-	
-	-- Use the following variables to adjust the color of the city the player is in (without there being a street around them)
-	town_r = 255
-	town_g = 255
-	town_b = 255
-	town_a = 255
-	
-	-- Determine rather the HUD should only display when player(s) are inside a vehicle or not
-	checkForVehicle = true
+-- Use the following variables to adjust the color of the border around direction.
+border_r = 255
+border_g = 255
+border_b = 255
+border_a = 100
+
+-- Use the following variables to adjust the color of the direction user is facing.
+dir_r = 255
+dir_g = 255
+dir_b = 255
+dir_a = 255
+
+-- Use the following variables to adjust the color of the street user is currently on.
+curr_street_r = 240
+curr_street_g = 200
+curr_street_b = 80
+curr_street_a = 255
+
+-- Use the following variables to adjust the color of the street around the player. (this will also change the town the user is in)
+str_around_r = 255
+str_around_g = 255 
+str_around_b = 255
+str_around_a = 255
+
+-- Use the following variables to adjust the color of the city the player is in (without there being a street around them)
+town_r = 255
+town_g = 255
+town_b = 255
+town_a = 255
+
+-- Determine rather the HUD should only display when player(s) are inside a vehicle or not
+checkForVehicle = true
+
+-- Hide street and area name from the bottom right of the hud
+hideStreetNameHUD = true
