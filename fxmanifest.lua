2464fa8f0a4c02d8573d525c50d51b15d6c10267
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2020-11-11 10:25:22 -0500

    Fixed ugly syntax

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,14 +8,14 @@ version '1.1.0'
 
 -- Client Scripts
 client_scripts {
-	'config.lua',
-	'client.lua'
+  'config.lua',
+  'client.lua'
 }
 
 ui_page('html/index.html')
 
 files({
-    'html/index.html',
-    'html/listener.js',
-    'html/style.css'
+  'html/index.html',
+  'html/listener.js',
+  'html/style.css'
 })
