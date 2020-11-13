f1695d58a5a9759b0f376a26afb1f240b8231ae1
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2020-11-12 23:40:25 -0500

    More customization options!

--- config.lua
+++ config.lua
@@ -1,4 +1,33 @@
--- This will be of more use later on...
+config = {
+	-- Color customization options
+	colours = {
+		current = {
+			r=255;
+			g=255;
+			b=255;
+			a=0.9;
+		};
+		crossing = {
+			r=240;
+			g=220;
+			b=80;
+			a=0.9;
+		};
+		direction = {
+			r=240;
+			g=220;
+			b=80;
+			a=0.9;
+		};
+	};
+	
+	-- General HUD positioning/sizing
+	position = {
+		offsetX = 0; -- Recommended to increment by x
+		offsetY = 0; -- Recommended to increment by x
+		size = 100;
+	};
 
--- Determine rather the HUD should only display when player(s) are inside a vehicle or not
-checkForVehicle = false
+	vehicleCheck = true; -- Rather or not to display HUD when player(s) are inside a vehicle
+	view = 'classic'; -- classic: original Street Label look -- dynamic: new dynamic inline HUD 
+}
