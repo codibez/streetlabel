5b0a2b2f2c29d9d9f18f85ca326e3e110a242916
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2020-11-13 09:53:19 -0500

    Functional color variables!

--- html/listener.js
+++ html/listener.js
@@ -20,43 +20,32 @@ function onMessageRecieved(event){
   }
   
   if (item && item.type === 'data') {
-		/* color customization */
-		let direction = [item.direction.r, item.direction.g, item.direction.b, item.direction.a];
+    /* color customization */
+    let border = [item.border.r, item.border.g, item.border.b, item.border.a];
+    let borderDOM = document.querySelectorAll('#border');
+    let direction = [item.direction.r, item.direction.g, item.direction.b, item.direction.a];
 		let zone = [item.zone.r, item.zone.g, item.zone.b, item.zone.a];
 		let street = [item.street.r, item.street.g, item.street.b, item.street.a];
-		let directionDOM = $('#direction'); let zoneDOM = $('#zone'); let streetDOM = $('#street');
-		
-		// Initialize DOM-elements with proper styling elements!
-		directionDOM.css('color', 'rgba('+direction.join(', ')+')');
-		zoneDOM.css('color', 'rgba('+zone.join(', ')+')');
-		streetDOM.css('color', 'rgba('+street.join(', ')+')');
-		
-		if (directionDOM.css('color').toString() !=='rgba('+direction.join(', ')+')'.toString()) {
-			console.log('ERROR => directionDOM styles are not configured properly :(');
-			console.log('CONFIGURED => rgba('+direction.join(', ')+')'.toString());
-			console.log('SET =>', directionDOM.css('color').toString());
-			
-			directionDOM.css('color', 'rgba('+direction.join(', ')+')');
-		} else { console.log('SUCCESS! => direction is properly configured!'); }
-		
-		if (zoneDOM.css('color').toString() !== 'rgba('+zone.join(', ')+')'.toString()) {
-			console.log('ERROR => zoneDOM styles are not configured properly :(');
-			console.log('CONFIGURED => rgba('+zone.join(', ')+')'.toString());
-			console.log('SET =>', zoneDOM.css('color').toString());
-			
-			zoneDOM.css('color', 'rgba('+zone.join(', ')+')');
-		} else { console.log('SUCCESS! => zone is properly configured!'); }
-		
-		if (streetDOM.css('color').toString() !== 'rgba('+street.join(', ')+')'.toString()) {
-			console.log('ERROR => zoneDOM styles are not configured properly :(');
-			console.log('CONFIGURED => rgba('+street.join(', ')+')'.toString());
-			console.log('SET =>', streetDOM.css('color').toString());
-			
-			streetDOM.css('color', 'rgba('+street.join(', ')+')');
-		} else { console.log('SUCCESS! => street is properly configured!'); }
-		
-		/* position/sizing */
-		
+    
+    // jQuery #direction to proper color & font-size configuration
+    $('#direction').css('color', 'rgba('+direction.join(', ')+')');
+		$('#direction').css('font-size', item.direction.size + 'vw');
+
+    // jQuery #street to proper color & font-size configuration
+    $('#street').css('color', 'rgba('+street.join(', ')+')');
+    $('#street').css('font-size', item.street.size + 'vw');
+    
+    // jQuery #zone to proper color & font-size configuration
+    $('#zone').css('color', 'rgba('+zone.join(', ')+')');
+		$('#zone').css('font-size', item.zone.size + 'vw');
+    
+    for (let i=0; i<borderDOM.length; i++) {
+      borderDOM[i].style.color = 'rgba('+border.join(', ')+')';
+      borderDOM[i].style.fontSize = item.border.size + 'vw';
+    }
+    
+    /* HUD position */
+    
 		/* view */
 	}
 }
