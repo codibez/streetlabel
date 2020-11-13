9d204ffdcbd11aba440f2f1c0aea605cbd370a0c
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2020-11-13 09:51:15 -0500

    Cleaned up syntax

--- config.lua
+++ config.lua
@@ -1,33 +1,42 @@
 config = {
-	-- Color customization options
-	colours = {
-		current = {
-			r=255;
-			g=255;
-			b=255;
-			a=0.9;
-		};
-		crossing = {
-			r=240;
-			g=220;
-			b=80;
-			a=0.9;
-		};
-		direction = {
-			r=240;
-			g=220;
-			b=80;
-			a=0.9;
-		};
-	};
-	
-	-- General HUD positioning/sizing
-	position = {
-		offsetX = 0; -- Recommended to increment by x
-		offsetY = 0; -- Recommended to increment by x
-		size = 100;
-	};
+  -- Customization options
+  border = {
+    r = 255;
+    g = 255;
+    b = 255;
+    a = 0.65;
+    size = 5;
+  };
 
-	vehicleCheck = true; -- Rather or not to display HUD when player(s) are inside a vehicle
-	view = 'classic'; -- classic: original Street Label look -- dynamic: new dynamic inline HUD 
+  current = {
+    r = 255;
+    g = 255;
+    b = 255;
+    a = 0.9;
+    size = 2.5;
+  };
+
+  crossing = {
+    r = 240;
+    g = 220;
+    b = 80;
+    a = 0.9;
+    size = 2;
+  };
+
+  direction = {
+    r = 240;
+    g = 220;
+    b = 80;
+    a = 0.9;
+    size = 4;
+  };
+
+  position = {
+    -- 0:100
+    offsetX = 17;
+    offsetY = 2.5; 
+  };
+
+  vehicleCheck = true; -- Rather or not to display HUD when player(s) are inside a vehicle
 }
