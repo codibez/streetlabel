a842b6765ecbfa983b64f5418e2c3b137a95ac34
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2022-08-03 09:54:47 -0400

    removed dynamic.css + v1.1.3
    Updated fxmanifest.lua accordingly to match proper version + removed the dynamic.css file.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,9 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'lowheartrate & Slyforn'
+author 'lowheartrate'
 description 'Street Label by lowheartrate'
-version '1.1.0'
+version '1.1.3'
 
 -- Client Scripts
 client_scripts {
@@ -16,8 +16,7 @@ ui_page('html/index.html')
 
 files({
 	'html/index.html',
-	'html/listener.js',
 	'html/styles.css',
   'html/font-face.css',
-  'html/dynamic.css'
+	'html/listener.js',
 })
