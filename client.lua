10af4329775b618538d915fc3aaeaf7465e8e2f0
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2022-08-03 09:58:20 -0400

    Update client.lua
    Cleaned up LUA code to perform much more proficiently on the client-side.

--- client.lua
+++ client.lua
@@ -7,77 +7,69 @@ local directions = {
   S = 180,
   SW = 135,
   W = 90,
-  NW = 45
-  --  N = 0, <= will result in the HUD breaking above 315deg
+  NW = 45,
 }
 
 local veh = 0;
-local hash1, hash2, heading;
 
 Citizen.CreateThread(function()
-
-	-- Wait a single second before sending data NUI message :? 
-	Citizen.Wait(1000);
-
-	SendNUIMessage({
-		type = 'streetLabel:DATA',
-    color = config.color,
-		offsetX = config.position.offsetX,
-		offsetY = config.position.offsetY,
-    scale = config.scale,
-    dynamic = config.dynamic
-	});
-
 	while true do
 		local ped = GetPlayerPed(-1);
 		local veh = GetVehiclePedIsIn(ped, false);
 
-		local coords = GetEntityCoords(PlayerPedId());
+		local coords = GetEntityCoords(ped);
 		local zone = GetNameOfZone(coords.x, coords.y, coords.z);
-		local zoneLabel = GetLabelText(zone);
 
-		if(config.vehicleCheck == false or veh ~= 0) then 
-			local var1, var2 = GetStreetNameAtCoord(coords.x, coords.y, coords.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
-			hash1 = GetStreetNameFromHashKey(var1);
-			hash2 = GetStreetNameFromHashKey(var2);
-			heading = GetEntityHeading(PlayerPedId());
+		local var1, var2 = GetStreetNameAtCoord(coords.x, coords.y, coords.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
+    local hash1 = GetStreetNameFromHashKey(var1);
+		local hash2 = GetStreetNameFromHashKey(var2);
+		local heading = GetEntityHeading(PlayerPedId());
 			
-			for k, v in pairs(directions) do
-				if (math.abs(heading - v) < 22.5) then
-					heading = k;
-		  
-					if (heading == 1) then
-						heading = 'N';
-						break;
-					end
+    for k, v in pairs(directions) do
+      if (math.abs(heading - v) < 22.5) then
+        heading = k;
+    
+        if (heading == 1) then
+          heading = 'N';
+          break;
+        end
 
-					break;
-				end
-			end
+        break;
+      end
+    end
 
-			local street2;
-			if (hash2 == '') then
-				street2 = zoneLabel;
-			else
-				street2 = hash2..', '..zoneLabel;
-			end
+    local street2;
+    if (hash2 == '') then
+      street2 = GetLabelText(zone);
+    else
+      street2 = hash2..', '..GetLabelText(zone);
+    end
 
+    if (config.vehicleCheck == false) then
 			SendNUIMessage({
-				type = 'streetLabel:MSG',
+				type = 'open',
 				active = true,
 				direction = heading,
 				street = hash1,
 				zone = street2
-			});
+			})
 		else
-			SendNUIMessage({
-				type = 'streetLabel:MSG',
-				active = false
-			});
+			if (veh ~= 0) then
+        SendNUIMessage({
+          type = 'open',
+          active = true,
+          direction = heading,
+          street = hash1,
+          zone = street2
+        })
+      else
+        SendNUIMessage({
+          type = 'open',
+          active = false
+        })
+      end
 		end
 		
-		-- Wait for half a second.
-		Citizen.Wait(500);
-		
+		Citizen.Wait(1000); -- 1s delay
 	end
 end)
