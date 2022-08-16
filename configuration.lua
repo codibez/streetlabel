33f43acc98ab41e84b194c2ee17a4f6bf6e7ff70
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2022-08-16 13:41:08 -0400

    Added `position.x` `position.y` to configuration.lua
    AGAIN... we'll see how this goes :+1:

--- configuration.lua
+++ configuration.lua
@@ -1,4 +1,8 @@
 config = {
 	color = {r = 240, g = 200, b = 80},
+	position = {
+		x = 17.55, -- distance across the x-axis, from left to right (default: 17.55)
+		y = 3,	   -- distance across the y-axis, from bottom to top (default: 3)
+	},
 	vehicleCheck = true,
 }
