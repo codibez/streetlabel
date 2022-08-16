aebddb7252d4e536dd495f9ed0a3641f2fd2fbff
Codi Bezouka-Smith <8846438+codibez@users.noreply.github.com>
2022-08-16 14:19:29 -0400

    Update listener.js
    Adjust positioning according to configuration file.

--- html/js/listener.js
+++ html/js/listener.js
@@ -2,15 +2,18 @@ window.addEventListener('message', (event) => {
 	let item = event.data;
   
 	if (item.type === 'open') {
-    if (!item.active) {
+		if (!item.active) {
 			$(".compass").hide();
 		} else {
-			$(".compass").show();
+			$('.compass').show();
+
+			$('.compass').css('left', item.positionX + '%');
+			$('.compass').css('bottom', item.positionY + '%');
 
 			$('span.direction').text(item.direction);
 			$('span.street').text(item.street);
 			$('span.street').css('color', item.color);
-      $('span.zone').text(item.zone);
+			$('span.zone').text(item.zone);
 		}
 	}  
 })
